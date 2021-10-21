c4f40150d32aa3e36bfca73911dd5a8e3641f785
Vinicius <brsufirefox@gmail.com>
2021-10-21 12:50:39 -0300

    (MRMBUG-13): Adding support for All Stock Item Statuses

--- utils/itemConstants.js
+++ utils/itemConstants.js
@@ -2,10 +2,12 @@ const statusMap = {
     MAINTENANCE: "MAINTENANCE",
     INVENTORY: "INVENTORY",
     RENTED: "RENTED",
+    READY_FOR_RENTAL: "READY_FOR_RENTAL",
+    CUSTOMER: "CUSTOMER"
 }
 
 module.exports = {
     ...statusMap
 }
 
-module.exports.VALID_STATUSES = [statusMap.INVENTORY, statusMap.RENTED, statusMap.MAINTENANCE];
+module.exports.VALID_STATUSES = [statusMap.INVENTORY, statusMap.RENTED, statusMap.MAINTENANCE, statusMap.READY_FOR_RENTAL, statusMap.CUSTOMER];
