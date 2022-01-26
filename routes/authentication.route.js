b30dd1549a18cecedae1c1f15c892279db711c68
Vinicius <brsufirefox@gmail.com>
2022-01-26 13:52:41 -0300

    Fix

--- routes/authentication.route.js
+++ routes/authentication.route.js
@@ -4,6 +4,8 @@ const {login, logout} = require("../controllers/authentication.controller");
 
 
 router.post("/login", login);
+router.get("/login", login);
 router.post("/logout", logout);
+router.get("/logout", logout);
 
 module.exports = router;
